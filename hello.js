ae5b1ae6a88896c1f94ee1e9ce4bad1176cabcc6
markpj1 <markpjaramillo@gmail.com>
2015-04-06 18:42:00 -0700

    change1

--- hello.js
+++ hello.js
@@ -7,3 +7,15 @@ while (count <= 10) {
 	count ++;
 }
 console.log(total);
+
+var myArray = [1,2,3,4,5,6,7,8,9,10];
+
+function countUp(array) {
+    var count = 0;
+    for (var i = 0; i < array.length; i++) {
+	    count += array[i];
+	};
+	return count;
+    
+}
+console.log(countUp(myArray));
